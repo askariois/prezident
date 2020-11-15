c5b7b18a92be967647d94add9fbc425774e9cc56
Askar <askar_1996@list.ru>
2020-11-15 13:15:39 +0600

    end three block

--- js/custom.js
+++ js/custom.js
@@ -26,3 +26,36 @@ let tab = function() {
 
 
 tab();
+
+// Slick slider
+$('.gallery__sl').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+// Slick slider
